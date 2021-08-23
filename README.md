2ab41935b732ceb3627fe34f185601aec9e4e838
RBianchi-Al <renatalebianchi@gmail.com>
2021-08-23 20:46:07 +0000

    post

---
+++ README.md
@@ -0,0 +1,7 @@
+Essa será uma aplicação para gerenciar tarefas (em inglês *todos*). Será permitida a criação de um usuário com `name` e `username`, bem como fazer o CRUD de *todos*:
+
+-[] Criar um novo *todo*;
+-[] Listar todos os *todos*;
+-[] Alterar o `title` e `deadline` de um *todo* existente;
+-[] Marcar um *todo* como feito;
+-[] Excluir um *todo*;
--- src/index.js
+++ src/index.js
@@ -1,21 +1,38 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+//gerando BD fake para armazenar dados
+const users = [];
 
+//midleware
 function checksExistsUserAccount(request, response, next) {
   // Complete aqui
 }
 
+// novo usuário (nome, username, id, todos)
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const {name, username} = request.body;
+
+  const usersAlreadyExists = users.some((user) => user.name === name)
+  
+  if(usersAlreadyExists){
+    return response.status(400).json({error: "Use Already Exists"})
+  }
+
+  users.push({
+    name, 
+    username,
+    id: uuidv4(),
+    todos: []
+  })
+  return response.status(201).json({message:"Successfully registered user"})
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
